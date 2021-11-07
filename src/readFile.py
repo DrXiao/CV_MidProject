fd7811a47cb679dcb78f102bab3b861a5eb8c5e2
Dr.Xiao <zxc71230@gmail.com>
2021-11-07 18:46:51 +0800

    Modify implementation of searching methods

--- src/readFile.py
+++ src/readFile.py
@@ -2,17 +2,19 @@ import os
 from pathlib import Path
 from search import *
 import random
+from numba import cuda
 
+filesize = 10000
 # 取得路徑
 path_norm = os.path.abspath(os.path.join(
-    os.getcwd(), os.path.pardir)) + "//norm.txt"
+    os.getcwd(), os.path.pardir)) + "/MidProject/norm.txt"
 path_unnorm = os.path.abspath(os.path.join(
-    os.getcwd(), os.path.pardir)) + "//unnorm.txt"
+    os.getcwd(), os.path.pardir)) + "/MidProject/unnorm.txt"
 
 #############################
 #  data_XX[][0]→檔案名稱     #
 #  data_XX[][1]→類別         #
-#  data_XX[][2:223]→特徵值   #
+#  data_XX[][2]→特徵值   #
 ##############################
 
 
@@ -22,7 +24,7 @@ def readFeature(path):
         cnt = 0
         for line in f.readlines():
             s = line.split(' ')  # 共224項
-            data.append(s)
+            data.append([s[0], s[1], np.array([float(num) for num in s[2:]])])
     # print(data_norm[0])
     return data
 
@@ -30,7 +32,7 @@ def readFeature(path):
 def distance(num, data, c):
     d = []
 
-    for i in range(10000):
+    for i in range(filesize):
         if i == num:
             if c == 0:
                 d.append(1E9)
@@ -39,13 +41,13 @@ def distance(num, data, c):
             continue
         if c == 0:  # 尤拉 公式
             d.append(euclidean_distance(
-                data[num][2:223], data[i][2:223]))
+                data[num][2], data[i][2]))
         elif c == 1:  # cosine 公式
             d.append(consine_distance(
-                data[num][2:223], data[i][2:223]))
+                data[num][2], data[i][2]))
         elif c == 2:  # pcc公式
             d.append(pcc_distance(
-                data[num][2:223], data[i][2:223]))
+                data[num][2], data[i][2]))
     # print(d)
     return d
 
@@ -66,34 +68,59 @@ def searching(caculate, value):
     return idx
 
 
-def main():
-
-    # 隨便選一個編號，顯示所有類別的第pic張圖片，顯示在前端上 (rand_pic+200*k)
-    rand_pic = random.randint(0, 199)
-    print("rand: ", rand_pic)
-    k = random.randint(0, 49)   # 顯示的50張中，被選擇的那一張(從前端接)
-    print("idx: ", k*200 + rand_pic)
-    caculate = 1  # 計算方式
-    flag_norm = True  # 是否正規化
+def finding(pic_num, data, flag_dist):
     value = []  # 所有距離
     similar_idx = []  # 最大的10個距離的index
 
-    if flag_norm:
-        data = readFeature(path_norm)
-    else:
-        data = readFeature(path_unnorm)
+    value = distance(pic_num, data, flag_dist)
 
-    value = distance(k*200+rand_pic, data, caculate)
-
-    similar_idx = searching(caculate, value)
-    print("original picture: ", data[(k*200 + rand_pic)][0])
-    print(similar_idx)
-    correct = 0
+    similar_idx = searching(flag_dist, value)
+    # print("original picture: ", data[pic_num][0])
+    # print(similar_idx)
+    accuracy = 0
     for i in range(10):
-        print(data[similar_idx[i]][0])
-        if(data[similar_idx[i]][1] == data[k*200+rand_pic][1]):
-            correct += 1
-    print("\ncorrect = ", correct*10, "%")
+        # print(data[similar_idx[i]][0])
+        if(data[similar_idx[i]][1] == data[pic_num][1]):
+            accuracy += 1
+    # print("\ncorrect = ", correct*10, "%")
+    return accuracy
+
+
+def main():
+
+    k = random.randint(0, 49)   # 選擇類別)
+    rand_pic = random.randint(0, 199)  # 選擇編號
+    # print("rand: ", rand_pic)
+    # print("idx: ", k*200 + rand_pic)
+
+    caculate = 1  # 計算方式
+    flag_norm = True  # 是否正規化
+    # accuracy = 0
+    # accuracy = finding(k*200 + rand_pic, flag_norm, caculate)
+
+    '''下面是用來測準確率ㄉ！'''
+    f = open("accuracy.txt", "w")
+    norm_data = readFeature(path_norm)
+    unnorm_data = readFeature(path_unnorm)
+    data = [norm_data, unnorm_data]
+    for k in range(2):      # 是否正規化
+        if k:
+            f.write("\n***normalize***\n")
+        else:
+            f.write("\n ***unnormalize***\n")
+        for i in range(3):              # 不同公式
+            accuracy = 0
+            for j in range(filesize):      # 跑10000張圖片
+                print("pic = ", j, ", caculate = ",
+                      i, "accuracy = ", accuracy)
+                accuracy += finding(j, data[k], i)
+            if i == 0:
+                str1 = "Eudience distance: " + str(accuracy/1000) + "\n"
+            elif i == 1:
+                str1 = "Cosine distance: " + str(accuracy/1000) + "\n"
+            else:
+                str1 = "PCC distance: " + str(accuracy/1000) + "\n"
+            f.write(str1)
 
 
 if __name__ == '__main__':
--- src/search.py
+++ src/search.py
@@ -1,65 +1,72 @@
-import typing
-import os
 import numpy as np
-
 
 def euclidean_distance(vec1, vec2):
     # 歐幾里得距離: 越小越相近
-    feature_num = len(vec1)
-    sum = 0
+    _sum = sum(((vec1 - vec2) ** 2))
 
+    """
     for feature_index in range(feature_num):
-        x = float(vec1[feature_index])
-        y = float(vec2[feature_index])
+        x = (vec1[feature_index])
+        y = (vec2[feature_index])
         sum = sum + (x - y)**2
-
-    Eucl_similarity = sum**0.5  # 最後的歐幾里得距離
+    """
+    Eucl_similarity = _sum**0.5  # 最後的歐幾里得距離
 
     return Eucl_similarity
 
 
 def consine_distance(vec1, vec2):
     # Cosine Similarity: 餘弦值越接近於1代表夾角越接近於0，向量上就越相似
-    feature_num = len(vec1)
     numerator = 0  # 分子
     denominator = 0  # 分母
-    A = 0
-    B = 0
+    numerator = sum(vec1 * vec2)
+    denominator = (sum(vec1 ** 2) ** 0.5) * (sum(vec2 ** 2) ** 0.5)
 
+    """
     for feature_index in range(feature_num):
-        numerator = float(vec1[feature_index]) * \
-            float(vec2[feature_index]) + numerator
-        A = float(vec1[feature_index])**2 + A  # sigma (vec1)**2
-        B = float(vec2[feature_index])**2 + B  # sigma (vec2)**2
+        numerator = (vec1[feature_index]) * \
+            (vec2[feature_index]) + numerator
+        A = (vec1[feature_index])**2 + A  # sigma (vec1)**2
+        B = (vec2[feature_index])**2 + B  # sigma (vec2)**2
     denominator = (A**0.5) * (B**0.5)
-    consine_similarity = float(numerator/denominator)  # 最後cosine距離
+    """
+    consine_similarity = (numerator/denominator)  # 最後cosine距離
 
     return consine_similarity
 
 
 def pcc_distance(vec1, vec2):
     # Pearson Correlation Coefficient:相關係數越高代表兩項目之間的關係越密切，反之則越不相關
-    feature_num = len(vec1)
     average1 = 0
     average2 = 0
+    """
     for i in range(feature_num):
-        average1 = average1 + float(vec1[i])
-        average2 = average2 + float(vec2[i])
+        average1 = average1 + (vec1[i])
+        average2 = average2 + (vec2[i])
     average1 = average1/feature_num  # vec1的平均
     average2 = average2/feature_num  # vec2的平均
+    """
+    average1 = np.average(vec1)
+    average2 = np.average(vec2)
 
     numerator = 0  # 分子
     denominator = 0  # 分母
     part1 = 0  # sigma (vec1 - average1)^2
     part2 = 0  # sigma (vec2 - average2)^2
 
+    """
     for feature_index in range(feature_num):
         numerator = numerator + \
-            (float(vec1[feature_index]) - average1) * \
-            (float(vec2[feature_index])-average2)
-        part1 = (float(vec1[feature_index]) - average1)**2 + part1
-        part2 = (float(vec2[feature_index])-average2)**2 + part2
+            ((vec1[feature_index]) - average1) * \
+            ((vec2[feature_index])-average2)
+        part1 = ((vec1[feature_index]) - average1)**2 + part1
+        part2 = ((vec2[feature_index])-average2)**2 + part2
     denominator = (part1 ** 0.5)*(part2 ** 0.5)
-    pcc_similarity = float(numerator / denominator)  # 最後求得的PCC距離
+    """
+    numerator = sum((vec1 - average1) * (vec2 - average2))
+    denominator = (sum((vec1-average1) ** 2) ** 0.5) * \
+        (sum((vec2-average2) ** 2) ** 0.5)
+
+    pcc_similarity = (numerator / denominator)  # 最後求得的PCC距離
 
     return pcc_similarity
