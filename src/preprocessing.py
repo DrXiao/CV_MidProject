68bc4d40904ab4ad0f927188af971f6a215b2838
Dr.Xiao <zxc71230@gmail.com>
2021-11-12 16:38:43 +0800

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -19,22 +19,23 @@ for feature_file in os.listdir(features_path):
             #print(img_info[479])
             if img_info[479].find(".db") != -1:
                 continue
-            img_features = [int(feature) for feature in img_info[0:80] + img_info[336:478]]
+            img_features = [float(feature) for feature in img_info[0:80] + img_info[336:478]]
             all_imgs_features[img_info[479]] = [feature_file.split(".")[0], img_features]
 
 with open("unnorm.txt", "w") as file:
     for img in all_imgs_features:
         print("%s %s"%(img, all_imgs_features[img][0]), end='', file=file)
-        max_feature = max(all_imgs_features[img][1])
-        min_feature = min(all_imgs_features[img][1])
         for feature_idx in range(len(all_imgs_features[img][1])):
             print(" %d"%(all_imgs_features[img][1][feature_idx]), end='', file=file)
-            all_imgs_features[img][1][feature_idx] = (all_imgs_features[img][1][feature_idx] - min_feature) / (max_feature - min_feature)
         print(file=file)
 
 with open("norm.txt", "w") as file:
     for img in all_imgs_features:
+        max_feature = max(all_imgs_features[img][1])
+        min_feature = min(all_imgs_features[img][1])
+        print(max_feature, min_feature)
         print("%s %s"%(img, all_imgs_features[img][0]), end='', file=file)
         for feature in all_imgs_features[img][1]:
-            print(" %f"%(feature), end='', file=file)
+            feature = (feature - min_feature) / (max_feature - min_feature)
+            print(" %.32f"%(feature), end='', file=file)
         print(file=file)
